7a59045b27030cce46cb8311bcbf02b6354783ac
Roshan <support@technofra.com>
2026-05-15 16:05:33 +0530

    brand naming page added

--- layouts/header.php
+++ layouts/header.php
@@ -225,7 +225,7 @@
                         <li><a href="visual-brand-language.php">Visual Brand Language</a></li>
                         <li><a href="brand-architecture.php">Brand Architecture</a></li>
                         <li><a href="packaging-design.php">Packaging Design</a></li>
-                        <li><a href="#">Brand Naming</a></li>
+                        <li><a href="brand-naming.php">Brand Naming</a></li>
                         <li><a href="#">Communication Design</a></li>
                         <li><a href="#">Brand Tonality & Messaging</a></li>
                         <li><a href="#">Logo Design & Brand Management</a></li>
